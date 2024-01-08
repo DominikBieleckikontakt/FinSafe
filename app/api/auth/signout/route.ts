627a0777b85980d317082c5c2c845afc51e39c3f
DominikBieleckikontakt <dominikbielecki.kontakt@gmail.com>
2024-01-08 21:10:35 +0100

    logout repaired

--- app/api/auth/signout/route.ts
+++
@@ -1,7 +0,0 @@
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  cookies().has("remember-me") && cookies().delete("remember-me");
-  return NextResponse.json({ message: "logged out" });
-}
