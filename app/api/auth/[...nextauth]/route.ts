bbd6da1527f28499bb5dda9db21069962c8cedba
DominikBieleckikontakt <dominikbielecki.kontakt@gmail.com>
2024-01-08 20:18:35 +0100

    nextauth handler done

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,12 @@
 import { authOptions } from "@/lib/auth";
-import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 import { cookies } from "next/headers";
 
 // const handler = NextAuth(authOptions);
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async () => {
   const maxAge = cookies().has("remember-me") ? 60 * 60 * 24 * 30 : 10;
-  return await NextAuth(req, res, {
+  return await NextAuth({
     ...authOptions,
     session: {
       strategy: "jwt",
